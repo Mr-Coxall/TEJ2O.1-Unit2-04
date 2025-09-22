18cc23388d7377895e5c334a9651a03b5617d8f0
Patrick Coxall <mr.coxall@mths.ca>
2025-09-22 12:01:03 -0400

    init

--- main.ts
+++ main.ts
@@ -5,5 +5,15 @@
  * This program simulates a 6 sided dice
 */
 
+// our variable for a random number
+let randomNumber: number
+const someName: string = "Mr Coxall"
+
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
+
+// shows sad face
+input.onButtonPressed(Button.A, function() {
+    randomNumber = randint(1, 6)
+    basic.showNumber(randomNumber)
+})
